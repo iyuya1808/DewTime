a28b5c60765df53fa13ac36d0d9f9064da8a4b13
Yuya Itonaga <technophere@gmail.com>
2026-05-22 01:00:25 +0900

    Initial commit: DewTime iOS app
    Xcode project with initial app structure and test targets.

---
+++ DewTime/DewTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DewTimeApp.swift
+//  DewTime
+//
+//  Created by 糸長優矢 on 2026/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DewTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DewTimeTests/DewTimeTests.swift
@@ -0,0 +1,19 @@
+//
+//  DewTimeTests.swift
+//  DewTimeTests
+//
+//  Created by 糸長優矢 on 2026/05/22.
+//
+
+import Testing
+@testable import DewTime
+
+struct DewTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
